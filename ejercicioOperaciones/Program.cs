29607901bb13d948c15578936e15aed1c4be283f
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-05-10 17:47:31 -0600

    Se hicieron modificaciones dentro de las instrucciones del archivo Program.cs del ejercicioOperaciones

--- ejercicioOperaciones/Program.cs
+++ ejercicioOperaciones/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Realizar un programa en el cual se declaren dos valores enteros por teclado utilizando el método __init__.
+Realizar un programa en el cual se declaren dos valores enteros por teclado.
 Calcular después la suma, resta, multiplicación y división.
 Utilizar un método para cada una e imprimir los resultados obtenidos.
 Llamar a la clase Calculadora.
