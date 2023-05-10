0bb43f7da4ac0a37a5e5c50b63eb34589d3f0423
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-05-10 17:45:48 -0600

    Se modificó el nombre de la clase MayorEdad a Persona

---
+++ ejercicioMayorEdad/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ejercicioMayorEdad
+{
+    class Persona
+    {
+        // Atributos
+        private string nombre;
+        private int edad;
+
+        // Constructor
+        public MayorEdad(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        // Métodos
+        public void Imprimir()
+        {
+            Console.WriteLine($"El nombre del alumno es: {nombre}");
+            Console.WriteLine($"La edad del alumno es: {edad}");
+        }
+
+        public void Mayor()
+        {
+            if (edad >= 18)
+            {
+                Console.WriteLine("El alumno es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("El alumno es menor de edad");
+            }
+        }
+    }
+}
