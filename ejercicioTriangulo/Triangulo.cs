21e11a0422ba500ce321b2d58a7338a71a1b3667
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-05-12 16:31:41 -0600

    Se hicieron modificaciones en el archivo "Triangulo.cs", se agregaron las medidas en "cm"

--- ejercicioTriangulo/Triangulo.cs
+++ ejercicioTriangulo/Triangulo.cs
@@ -19,9 +19,9 @@ namespace ejercicioTriangulo
         // Métodos
         public void Imprimir()
         {
-            Console.WriteLine($"El lado 1 mide: {lado1}");
-            Console.WriteLine($"El lado 2 mide: {lado2}");
-            Console.WriteLine($"El lado 3 mide: {lado3}");
+            Console.WriteLine($"El lado 1 mide: {lado1}cm");
+            Console.WriteLine($"El lado 2 mide: {lado2}cm");
+            Console.WriteLine($"El lado 3 mide: {lado3}cm");
         }
 
         public void TipoTriangulo()
@@ -43,27 +43,27 @@ namespace ejercicioTriangulo
         {
             if (lado1 > lado2 && lado1 > lado3)
             {
-                Console.WriteLine($"El lado mayor es: lado1 ({lado1})");
+                Console.WriteLine($"El lado mayor es: lado1 ({lado1}cm)");
             }
             else if (lado2 > lado1 && lado2 > lado3)
             {
-                Console.WriteLine($"El lado mayor es: lado2 ({lado2})");
+                Console.WriteLine($"El lado mayor es: lado2 ({lado2}cm)");
             }
             else if (lado3 > lado1 && lado3 > lado2)
             {
-                Console.WriteLine($"El lado mayor es: lado3 ({lado3})");
+                Console.WriteLine($"El lado mayor es: lado3 ({lado3}cm)");
             }
             else if (lado1 > lado2 && lado3 > lado2)
             {
-                Console.WriteLine($"Los 2 lados mayores son: lado1 ({lado1}) y lado3 ({lado3})");
+                Console.WriteLine($"Los 2 lados mayores son: lado1 ({lado1}) y lado3 ({lado3}cm)");
             }
             else if (lado2 > lado1 && lado3 > lado1)
             {
-                Console.WriteLine($"Los 2 lados mayores son: lado2 ({lado2}) y lado3 ({lado3})");
+                Console.WriteLine($"Los 2 lados mayores son: lado2 ({lado2}cm) y lado3 ({lado3}cm)");
             }
             else if (lado1 > lado3 && lado2 > lado3)
             {
-                Console.WriteLine($"Los 2 lados mayores son: lado1 ({lado1}) y lado2 ({lado2})");
+                Console.WriteLine($"Los 2 lados mayores son: lado1 ({lado1}cm) y lado2 ({lado2}cm)");
             }
             else
             {
